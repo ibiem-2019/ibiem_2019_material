83700a157e8f1076be2a69e9b6b7238fdd7693e0
Josh Granek <josh@duke.edu>
2019-09-05 23:14:20 -0400

    reproducibility_demo.sh: incorporated test_docker_command_exec.sh (i.e. commandline run of demo script)

--- content/misc/reproducibility_demo.sh
+++ content/misc/reproducibility_demo.sh
@@ -1,24 +1,28 @@
+
+if ! [[ "$1" =~ ^(LOCAL|REMOTE|COMMAND)$ ]]; then
+    printf "Run with LOCAL or REMOTE or COMMAND\n"
+    printf "\tREMOTE: run RStudio\n"
+    printf "\tLOCAL: run RStudio with access restricted to a web browser on the same machine\n"
+    printf "\tCOMMAND: run demonstration dada2 and phyloseq pipeline, then exit\n"
+    exit 1
+else
+    REMOTE_OR_LOCAL=${1}
+fi
+
+
 DEMO_BASE="/tmp/reproducible_demo_`date +%s`_tmp"
 WORK_DIR="$DEMO_BASE/work"
 DATA_DIR="$DEMO_BASE/data"
 DOCKER_IMAGENAME="ibiem/docker_rstudio_ibiem2019"
 SEP_STRING="\n--------------------------------------------------\n"
 
-
-REMOTE_OR_LOCAL=${1:-"LOCAL"}
+# REMOTE_OR_LOCAL=$1
 # MY_PASSWORD=${2:-"BadPa55word"}
 MY_PASSWORD="`openssl rand -base64 16 | colrm 20`"
 
 HOST_PORT=8787
 
-if [ $REMOTE_OR_LOCAL == "REMOTE" ]; then
-    EXPOSE_PORT="${HOST_PORT}:8787"
-    URL="http://`hostname -A | cut -f1 -d' '`:${HOST_PORT}/"
-else
-    EXPOSE_PORT="127.0.0.1:${HOST_PORT}:8787"
-    URL="http://localhost:${HOST_PORT}/"
-fi
-echo "expose argument: ${EXPOSE_PORT}"
+
 
 
 
@@ -32,14 +36,32 @@ git clone https://github.com/ibiem-2019/ibiem_2019_material.git ${WORK_DIR}/demo
 printf "\n${SEP_STRING} Pulling docker image: $DOCKER_IMAGENAME ${SEP_STRING}"
 docker pull $DOCKER_IMAGENAME
 
-# 
-# printf "\n${SEP_STRING} Downloading data from DDS ${SEP_STRING}"
-# 
-# ddsclient download -p HTS_course --include hts_2019_data/hts2019_pilot_rawdata/21_2019_P_M1_S21_L002_R1_001.fastq.gz $DATA_DIR
-# 
-# 
-# printf "\n${SEP_STRING} Cloning repo from gitlab ${SEP_STRING}"
-# 
+
+if [ $REMOTE_OR_LOCAL == "COMMAND" ]; then
+    printf "\n${SEP_STRING} STARTING Pipeline in Docker ${SEP_STRING}"
+    docker run \
+	   -it \
+	   --rm \
+	   -v ${WORK_DIR}:/home/guest \
+	   -v ${DATA_DIR}:/data \
+	   $DOCKER_IMAGENAME \
+	   Rscript -e "rmarkdown::render('/home/guest/demo/content/lessons/run_everything.Rmd')"
+    printf "\n${SEP_STRING} FINISHED Pipeline ${SEP_STRING}"
+    exit 0
+elif [ $REMOTE_OR_LOCAL == "REMOTE" ]; then
+    EXPOSE_PORT="${HOST_PORT}:8787"
+    URL="http://`hostname -A | cut -f1 -d' '`:${HOST_PORT}/"
+elif [ $REMOTE_OR_LOCAL == "LOCAL" ]; then
+    EXPOSE_PORT="127.0.0.1:${HOST_PORT}:8787"
+    URL="http://localhost:${HOST_PORT}/"
+else
+    printf "Run with LOCAL or REMOTE or COMMAND\n"
+    printf "\tREMOTE: run RStudio\n"
+    printf "\tLOCAL: run RStudio with access restricted to a web browser on the same machine\n"
+    printf "\tCOMMAND: run demonstration dada2 and phyloseq pipeline, then exit\n"
+    exit 1
+fi
+echo "expose argument: ${EXPOSE_PORT}"
 
 
 printf "\n${SEP_STRING} Running Docker ${SEP_STRING}"
